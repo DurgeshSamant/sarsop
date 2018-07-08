a341b5be1bb9be0b4eaa8b21cc6ca2aa71b80f7f
DurgeshSamant <durgeshsamant1987@gmail.com>
2018-07-09 00:35:39 +0530

    removed undiscounted bound calculation

--- src/Algorithms/SARSOP/SampleBP.cpp
+++ src/Algorithms/SARSOP/SampleBP.cpp
@@ -286,12 +286,7 @@ cout << " | child.sval : " << newIndexRow.sval << ".row " << newIndexRow.row << 
 				//double curUb = bounds->upperBoundBVpair->getValue (currentNode.getNextState (i, j).s);
 				solver->beliefCacheSet[newIndexRow.sval]->getRow(newIndexRow.row)->UB = curUb; // beliefCache->getRow(newRow)->UB = curUb;
 				// 061008 changed calculation for obsProb
-        if(i>=0 && i<4){
 				nextNodeTargetUbArr[currentRoot] = ((curTargetUb - currentNode.Q[i].immediateReward) / problem->getDiscount() - futureValue) / (Qa.stateOutcomes[Xn]->outcomes[j]->obsProb) ;
-        }
-        else{
-				nextNodeTargetUbArr[currentRoot] = ((curTargetUb - currentNode.Q[i].immediateReward) / 1.0 - futureValue) / (Qa.stateOutcomes[Xn]->outcomes[j]->obsProb) ;
-        }
 				//nextNodeTargetUb = ((curTargetUb - currentNode.Q[i].immediateReward) / problem->getDiscount() - futureValue) / (Qa.stateOutcomes[Xn]->outcomes[j]->obsProb * Qa.stateOutcomes[Xn]->obsStateProb) ;
 				// the above takes the place of this
 				//nextNodeTargetUb = ((curTargetUb - currentNode.Q[i].immediateReward) / problem->getDiscount() - futureValue) / Qa.stateOutcomes[Xn]->outcomes[j]->obsProb;
@@ -502,13 +497,7 @@ cout << "End of one trial due to !( (currentNode.Q[i].ubVal == CB_QVAL_UNDEFINED
 			curTargetLb = max(maxValue, nextNodeTargetLbArr[currentRoot]);
 		}
 
-    if(action>=0 && action <4)
-    {
 		nextNodeTargetLbArr[currentRoot] = ((curTargetLb - currentNode.Q[action].immediateReward) / problem->getDiscount() - sum_o) / oxp;
-    }
-    else{
-		nextNodeTargetLbArr[currentRoot] = ((curTargetLb - currentNode.Q[action].immediateReward) / 1.0 - sum_o) / oxp;
-    }
 		//nextNodeTargetLb = ((curTargetLb - currentNode.Q[action].immediateReward) / problem->getDiscount() - sum_o) / opv(observation);
 
 		return (value > curTargetLb);
